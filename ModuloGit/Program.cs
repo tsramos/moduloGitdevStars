80980619ee4e68885005dc4184408c0e1dfb600c
Thiago Ramos <thiago.s.ramos@outlook.com>
2022-12-02 19:46:02 -0300

    Criado comentario da atividade

---
+++ ModuloGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ModuloGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Atividade:
+             * Crie um programa que receba dois valores e calcule a soma desses valores.
+             * Ao final exiba o resultado no console.
+             */
+        }
+    }
+}
