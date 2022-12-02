424d21f94a2a7e473c2f548e59e4f5d61dcc49e0
Thiago Ramos <thiago.s.ramos@outlook.com>
2022-12-02 20:33:02 -0300

    Criada estrura base do programa

--- ModuloGit/Program.cs
+++ ModuloGit/Program.cs
@@ -10,6 +10,15 @@ namespace ModuloGit
              * Crie um programa que receba dois valores e calcule a soma desses valores.
              * Ao final exiba o resultado no console.
              */
+
+            #region Base
+            Console.WriteLine("Digite 1 para iniciar o programa ou digite 0 para sair");
+            int opcao = int.Parse(Console.ReadLine());
+            while (opcao != 0)
+            {
+
+            }
+            #endregion
         }
     }
 }
